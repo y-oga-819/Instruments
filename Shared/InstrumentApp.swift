280391912ce7467bf618c026e2200583e452b424
yuki819 <yuki819@mba2018.local>
2022-07-09 23:27:40 +0900

    feature(Instrument): Xcodeプロジェクト生成

---
+++ Shared/InstrumentApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstrumentApp.swift
+//  Shared
+//
+//  Created by 小笠原佑樹 on 2022/07/09.
+//
+
+import SwiftUI
+
+@main
+struct InstrumentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
